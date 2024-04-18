93b23c40620d03b201c9f5b7cfa14359e06fb7e0
Maxim Slipenko <no-reply@maxim.slipenko.com>
2024-04-18 22:33:50 +0300

    Changes

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,10 @@
 const React = require("react");
 const QAPI = require("./qapi").default;
 const { RawHtml } = require("@quarkly/components");
- const insideHeadRawHtmls = [];
+ const insideHeadRawHtmls = [<link
+href="https://cdn.jsdelivr.net/npm/swiper@8.4.7/swiper-bundle.min.css"
+rel="stylesheet"
+/>];
 const beforeBodyRawHtmls = [];
  export const onRenderBody = (
     {
